d627e549d9fa7ca7407ae98341dadeab5aeba5a5
dhwani040906 <dhwani.040906@gmail.com>
2025-07-15 12:03:12 +0530

    pages

--- ocean/src/App.jsx
+++ ocean/src/App.jsx
@@ -1,15 +1,18 @@
 import Gallery from './Gallery';
 import Login from './Login'
 import Register from './Register';
+import Home from './Home';
 import React, {useState} from 'react';
 
 function App(){
+
     const [showLogin, setShowLogin] = useState(true);
 
     return(
         <div>
            {showLogin ? (<Login switchToRegister={() => setShowLogin(false)} />) : (
             <Register switchToLogin={() => setShowLogin(true)} />)}
+            {/* <Home/> */}
         </div>
     )
 }
