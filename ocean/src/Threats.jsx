747cfc5acdc4b1ec6133f98d1c6e84b3726d86af
dhwani040906 <dhwani.040906@gmail.com>
2025-07-22 01:08:21 +0530

    ..

--- ocean/src/Threats.jsx
+++ ocean/src/Threats.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-// import './Threats.css';
+import './Threats.css';
 
 const AquaticThreatsWebsite = () => {
   const [currentAnimal, setCurrentAnimal] = useState(0);
