dbd75a97faac44c7eb0a3e07eda267f55d0b4aa5
ASH <u24ai106@aid.svnit.ac.in>
2025-08-03 00:23:11 +0530

    backend

--- ocean/src/Register.jsx
+++ ocean/src/Register.jsx
@@ -1,24 +1,29 @@
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
+import { useState } from 'react';
+import axios from 'axios'
 
 function Register() {
     const navigate = useNavigate();
-
+    const [username,setusername] = useState("");
+    const [email,setemail] = useState("");
+    const [phone,setphone] = useState();
+    
     return (
         <div className="auth-wrapper">
             <div className="auth-box">
                 <h2>Sign Up</h2>
                 <label>
                     Username:
-                    <input type="text" name="username" />
+                    <input type="text" name="username" onChange={(e)=>{setusername(e.target)}}/>
                 </label>
                 <label>
                     Email:
-                    <input type="email" name="email" />
+                    <input type="email" name="email" onChange={(e)=>{setemail(e.target)}}/>
                 </label>
                 <label>
                     Phone No:
-                    <input type="tel" name="telephone" />
+                    <input type="tel" name="telephone" onChange={(e)=>{setphone(e.target)}}/>
                 </label>
                 <label>
                     Password:
